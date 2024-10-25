34b893b2cb4f921267ddc334e87ac85f256df507
Crypto <aksenovmaksimelan@gmail.com>
2024-10-25 19:04:34 +0300

    added one more hamster and coded it into, working properly on this commit

--- js/core.js
+++ js/core.js
@@ -4,6 +4,7 @@ const money = document.getElementById('money');
 
 const hamsterPoor = document.getElementById('poor-hamster');
 const hamsterRich = document.getElementById('rich-hamster');
+const hamsterSport = document.getElementById('sport-hamster');
 
 let currentCoin = 0
 
@@ -12,6 +13,9 @@ hamster.addEventListener('click', () => {
     money.innerText = currentCoin;
     if(currentCoin == 10) {
         hamsterPoor.style.display = 'none';
+        hamsterSport.style.display = 'block';
+    } else if(currentCoin == 20) {
+        hamsterSport.style.display = 'none';
         hamsterRich.style.display = 'block';
     }
 })

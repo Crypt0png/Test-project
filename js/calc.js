815d79779d184a43a8c291a85d12ecf316e2135f
Crypto <aksenovmaksimelan@gmail.com>
2024-11-04 19:32:04 +0300

    little fix

--- js/calc.js
+++ js/calc.js
@@ -2,7 +2,7 @@ function appendToDisplay(value) {
     const display = document.getElementById('display');
     const lastChar = display.value.slice(-1);
 
-    if (["+", "-", "*", "/"].includes(lastChar) && ["+", "-", "*", "/"].includes(value)) {
+    if (["+", "-", "*", "/", "."].includes(lastChar) && ["+", "-", "*", "/", "."].includes(value)) {
         return;
     }
     display.value += value;
